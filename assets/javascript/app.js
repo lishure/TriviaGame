cb067367b679c76cf4a9d7b169e19c6d9e6cc86e
Lishure <HUEY_LISA@YAHOO.COM>
2019-03-25 19:00:41 -0700

    Psuedocoded and added theme

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+//Create jumbotron with start button in html
+//Create onclick function for start button so page changes 
+var start = function () {
+    //when starts, show questionaire page,
+}
+//Need to make array of variables
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+var questions = "";
+var answer = "";
+//Need to make an array of questions and answers? Maybe
+
+//Need to use hide/show/next function
+//Make up a list of 10 questions with radio buttons
+//Allow only 1 radio button to be selected per question
+//Log correct/incorrect answers
+//Need to set timer, also show time left.
+$("#start").on("click", function() {
+    delayButtonAlert = setTimeout(function() {
+     //when time runs out, display end page
+    }, 30000);
+  });
+//When timer runs out, show end page- need show correct/incorrect/unanswered questions
+//Need a total of 3 pages. start page, questionaire page, and end page.
+
+
