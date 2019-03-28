c024f7e1e606413a4efa7a56868e1e3304ae7530
Lishure <HUEY_LISA@YAHOO.COM>
2019-03-27 21:48:32 -0700

    added doms and click functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,18 +9,49 @@ var incorrect = 0;
 var unanswered = 0;
 var questions = "";
 var answer = "";
+//Need to make DOM variables
+var startButton = document.getElementById("start-button");
+var newContainer = document.getElementsByClassName("container");
+var newJumbotron = document.getElementsByClassName("jumbotron");
+var newTimer = document.querySelector(".timer");
+
+//Make on click function using DOMS
+startButton.addEventListener("click", getNewContainer);
+function getNewContainer() {
+    var startTime = 5;
+    newContainer[0].style.display = "Initial";
+    newJumbotron[0].style.display = "none";
+    
+    var timer = setInterval(function() {
+        if (startTime === 1){
+            clearInterval(timer);
+            newTimer.innerHTML = 5;
+            //placeholder. need to create a third container
+            newJumbotron[0].style.display = "Initial";
+            
+        };
+        console.log(startTime -= 1);
+        newTimer.innerHTML = startTime;
+    }, 1000);
+};
 //Need to make an array of questions and answers? Maybe
 
 //Need to use hide/show/next function
+//Container is hidden in html, will use on click show function
+
+
+
 //Make up a list of 10 questions with radio buttons
 //Allow only 1 radio button to be selected per question
 //Log correct/incorrect answers
 //Need to set timer, also show time left.
-$("#start").on("click", function() {
-    delayButtonAlert = setTimeout(function() {
+//$("#start").on("click", function() {
+  //  $(".container").show();
+
+   // delayButtonAlert = setTimeout(function() {
      //when time runs out, display end page
-    }, 30000);
-  });
+    //}, 30000);
+  //});
 //When timer runs out, show end page- need show correct/incorrect/unanswered questions
 //Need a total of 3 pages. start page, questionaire page, and end page.
 
