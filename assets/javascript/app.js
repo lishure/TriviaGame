e1ab2bd7f4f2def784c856990f3d8357ffcd1923
Lishure <HUEY_LISA@YAHOO.COM>
2019-03-29 19:30:45 -0700

    Code doesnt work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 //Create jumbotron with start button in html
 //Create onclick function for start button so page changes 
 var start = function () {
-    //when starts, show questionaire page,
+  //when starts, show questionaire page,
 }
 //Need to make array of variables
 var correct = 0;
@@ -22,27 +22,41 @@ var endPage = document.getElementsByClassName("jumbotron-special");
 //Make on click function using DOMS
 startButton.addEventListener("click", getNewContainer)
 function getNewContainer() {
-    var startTime = 5;
-    // newContainer[0].style.display = "Initial";
-    newContainer[0].classList.remove("invisible");
+  var startTime = 5;
+  // newContainer[0].style.display = "Initial";
+  newContainer[0].classList.remove("invisible");
 
-    // newJumbotron[0].style.display = "none";
-    newJumbotron[0].classList.add("invisible");
+  // newJumbotron[0].style.display = "none";
+  newJumbotron[0].classList.add("invisible");
 
-    
-    var timer = setInterval(function() {
-        if (startTime === 1){
-            clearInterval(timer);
-            newTimer.innerHTML= "";
-            //placeholder. need to create a third container
-            //Make consistent. invisible/visible 
-            endPage[0].classList.remove("invisible");
-            newContainer[0].classList.add("invisible");
-            
-        };
-        console.log(startTime -= 1);
-        newTimer.innerHTML = startTime;
-    }, 1000);
+
+  var timer = setInterval(function () {
+    if (startTime === 1) {
+      clearInterval(timer);
+      newTimer.innerHTML = "";
+      //placeholder. need to create a third container
+      //Make consistent. invisible/visible 
+      endPage[0].classList.remove("invisible");
+      newContainer[0].classList.add("invisible");
+
+    };
+    console.log(startTime -= 1);
+    newTimer.innerHTML = startTime;
+  }, 1000);
+};
+
+//If value = 0 is selected, then incorrect ++, else if value = 1 is selected,
+//then correct ++. When timer runs out, unanswered === 10 -(incorrect)-(correct)
+document.getElementById("radio").click()
+function addValues() {
+  if (value === 0) {
+    incorrect++;
+    console.log(incorrect)
+  }
+  else if (value === 1) {
+    correct++;
+    console.log(correct);
+  }
 };
 
 
@@ -52,7 +66,7 @@ function getNewContainer() {
 
 
 
-//Make up a list of 10 questions with radio buttons
+//Make up a list of 10 questions with radio buttons 
 //Allow only 1 radio button to be selected per question
 //Log correct/incorrect answers
 //Need to set timer, also show time left.
